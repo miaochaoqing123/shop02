53e8973957a48ff863446fed0c221d0137a5c799
缪超青 <419666193@qq.com>
2018-05-02 19:35:29 +0800

    'django的serializers序列化model'

--- MxShop/urls.py
+++ MxShop/urls.py
@@ -19,9 +19,15 @@ import xadmin
 from MxShop.settings import MEDIA_ROOT
 from django.views.static import serve
 
+from goods.views_base import GoodsListView
+
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+
+    # 商品列表页
+    url(r'^goods/$', GoodsListView.as_view(), name='goods-list')
+
 ]
 
 
---
+++ apps/goods/views_base.py
@@ -0,0 +1,46 @@
+from django.views.generic.base import View
+
+from goods.models import Goods
+# from django.views.generic import ListView
+
+class GoodsListView(View):
+    def get(self,request):
+        """
+        通过django的view实现商品列表页
+        :param request:
+        :return:
+        """
+        json_list = []
+        goods = Goods.objects.all()[:10]
+
+        # for good in goods:
+        #     json_dict = {}
+        #     json_dict['name'] = good.name
+        #     json_dict['category'] = good.category.name
+        #     json_dict['market_price'] = good.market_price
+        #     json_list.append(json_dict)
+
+        # from django.forms.models import model_to_dict  # 可以取代上面的代码,优化代码
+        # for good in goods:
+        #     json_dict = model_to_dict(good)
+        #     json_list.append(json_dict)
+
+        import json
+        from django.core import serializers  # 用于序列化
+        json_data = serializers.serialize('json', goods)
+        json_data = json.loads(json_data)
+        from django.http import HttpResponse, JsonResponse
+        # 当使用HttpResponse时,可以把上面的json_data = json.loads(json_data) 注释掉
+        # return HttpResponse(json_data, content_type='application/json')
+        return JsonResponse(json_data, safe=False)
+
+
+
+
+
+
+
+
+
+
+
