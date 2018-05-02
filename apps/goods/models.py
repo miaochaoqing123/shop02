27f2237b9ac55722a123d556990361cc71c21415
缪超青 <419666193@qq.com>
2018-05-02 17:32:07 +0800

    'add_goods_素材'

--- apps/goods/models.py
+++ apps/goods/models.py
@@ -33,7 +33,7 @@ class GoodsCategoryBrand(models.Model):
     category = models.ForeignKey(GoodsCategory, null=True, blank=True, verbose_name='商品类目')
     name = models.CharField(default='', max_length=30, verbose_name='品牌名', help_text='品牌名')
     desc = models.TextField(default='', max_length=200, verbose_name='品牌描述', help_text='品牌描述')
-    image = models.ImageField(max_length=200, upload_to='brand/images/')
+    image = models.ImageField(max_length=200, upload_to='brands/')
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
 
     class Meta:
@@ -59,7 +59,7 @@ class Goods(models.Model):
     goods_desc = UEditorField(verbose_name=u'内容', imagePath='goods/images/', width=1000, height=300,
                               filePath='goods/files/', default='')
     ship_free = models.BooleanField(default=True, verbose_name='是否承担运费')
-    goods_front_images = models.ImageField(upload_to='', null=True, blank=True, verbose_name='封面图片')
+    goods_front_images = models.ImageField(upload_to='goods/images/', null=True, blank=True, verbose_name='封面图片')
     is_new = models.BooleanField(default=False, verbose_name='是否新品')
     is_hot = models.BooleanField(default=False, verbose_name='是否热销')
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
