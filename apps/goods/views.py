4076ed304b6cffa9040284df3cd918fc97c56ab4
缪超青 <419666193@qq.com>
2018-05-03 12:34:25 +0800

    'GenericView方式实现商品列表页和分页功能'

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,28 +1,32 @@
+from rest_framework.pagination import PageNumberPagination
+
 from .serializers import GoodsSerializer
-from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework import status
 
 from .models import Goods
 
 # Create your views here.
 
 
-class GoodsListView(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
-    def get(self, request, format=None):
-        goods = Goods.objects.all()[:10]
-        goods_serializer = GoodsSerializer(goods, many=True)
-        return Response(goods_serializer.data)
+class GoodsPagination(PageNumberPagination):
+    """自定义page"""
+    page_size = 10
+    page_size_query_param = 'page_size'
+    page_query_param = 'p'
+    max_page_size = 100
 
-    # def post(self, request, format=None):
-    #     serializer = GoodsSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class GoodsListView(generics.ListAPIView):
+    """
+    商品列表项
+    """
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
+    pagination_class = GoodsPagination
+
+
+
 
 
 
