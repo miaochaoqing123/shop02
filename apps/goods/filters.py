89784b42db95fc49aefb46fa4af56d8e1471ebf0
缪超青 <419666193@qq.com>
2018-05-03 16:11:12 +0800

    'rest-framework的搜索设计'

--- apps/goods/filters.py
+++ apps/goods/filters.py
@@ -6,12 +6,12 @@ class GoodsFilter(django_filters.rest_framework.FilterSet):
     """商品的过滤类"""
     price_min = django_filters.NumberFilter(name='shop_price', lookup_expr='gte')
     price_max = django_filters.NumberFilter(name='shop_price', lookup_expr='lte')
-    # 对name进行模糊查询
-    name = django_filters.CharFilter(name='name', lookup_expr='icontains')
+    # # 对name进行模糊查询
+    # name = django_filters.CharFilter(name='name', lookup_expr='icontains')
 
     class Meta:
         model = Goods
-        fields = ['price_min', 'price_max', 'name']
+        fields = ['price_min', 'price_max']
 
 
 
